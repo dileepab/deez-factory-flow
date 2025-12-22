ec7b52f47d2056b0b68dd10fdb6fae0ccea022a1
postman me2b <postmanme2b@gmail.com>
2025-12-22 10:27:36 +0000

    A tree hydrated but some attributes of the server rendered HTML didn't m

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,39 +1,51 @@
 'use client';
 
-import React, { useMemo, type ReactNode, useEffect, useState } from 'react';
+import React, {
+  useMemo,
+  type ReactNode,
+  useEffect,
+  useState,
+} from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
 import { initializeFirebase } from '@/firebase';
 import { getCookie } from 'cookies-next';
-import { signInWithCustomToken, signOut as firebaseSignOut } from 'firebase/auth'; // Import signOut
+import {
+  signInWithCustomToken,
+  signOut as firebaseSignOut,
+} from 'firebase/auth'; // Import signOut
 
 interface FirebaseClientProviderProps {
   children: ReactNode;
 }
 
-export function FirebaseClientProvider({ children }: FirebaseClientProviderProps) {
-    const [isTokenSigningIn, setIsTokenSigningIn] = useState(true);
+export function FirebaseClientProvider({
+  children,
+}: FirebaseClientProviderProps) {
+  const [isClient, setIsClient] = useState(false);
+  const [isTokenSigningIn, setIsTokenSigningIn] = useState(true);
   const firebaseServices = useMemo(() => {
     // Initialize Firebase on the client side, once per component mount.
     return initializeFirebase();
   }, []);
 
   useEffect(() => {
+    setIsClient(true);
     const signInWithToken = async () => {
-        const token = getCookie('custom-token');
-        if (token && firebaseServices.auth.currentUser === null) {
-            try {
-                await signInWithCustomToken(firebaseServices.auth, token as string);
-            } catch (error) {
-                console.error("Failed to sign in with custom token", error);
-            }
+      const token = getCookie('custom-token');
+      if (token && firebaseServices.auth.currentUser === null) {
+        try {
+          await signInWithCustomToken(firebaseServices.auth, token as string);
+        } catch (error) {
+          console.error('Failed to sign in with custom token', error);
         }
-        setIsTokenSigningIn(false);
+      }
+      setIsTokenSigningIn(false);
     };
     signInWithToken();
   }, [firebaseServices.auth]);
 
-  if(isTokenSigningIn) {
-    return <div>Authenticating...</div>
+  if (!isClient || isTokenSigningIn) {
+    return <div>Authenticating...</div>;
   }
 
   return (
