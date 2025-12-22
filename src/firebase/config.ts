dd49fbdd0b658b2f6173a5fd137d3821556d869d
postman me2b <postmanme2b@gmail.com>
2025-12-22 09:29:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4207278931-f63d2",
+  "appId": "1:185576558602:web:9fc066d45b9e7f5f440ff3",
+  "apiKey": "AIzaSyAxbIYrOmR9szVrnoX9AgT3qad1e4FQeUE",
+  "authDomain": "studio-4207278931-f63d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "185576558602"
+};
